be5545e248d709896172a7e3fca66177473fe94f
Jun Li <junli19860919@gmail.com>
2021-03-21 12:22:04 -0700

    Update glow.py

--- models/flow/glow/glow.py
+++ models/flow/glow/glow.py
@@ -28,6 +28,8 @@ class Glow(nn.Module):
                            mid_channels=num_channels,
                            num_levels=num_levels,
                            num_steps=num_steps)
+        self.best_loss = 0
+        self.iter_step = 0
 
     def forward(self, x, reverse=False):
         if reverse:
@@ -68,6 +70,62 @@ class Glow(nn.Module):
         sldj = ldj.flatten(1).sum(-1)
 
         return y, sldj
+    
+@torch.enable_grad()
+def train_full(epoch, net, trainloader, device, optimizer, scheduler, loss_fn, max_grad_norm, iter_step):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    loss_meter = util.AverageMeter()
+    with tqdm(total=len(trainloader.dataset)) as progress_bar:
+        for x, _ in trainloader:
+            x = x.to(device)
+            optimizer.zero_grad()
+            z, sldj = net(x, reverse=False)
+            loss = loss_fn(z, sldj)
+            loss_meter.update(loss.item(), x.size(0))
+            loss.backward()
+            if max_grad_norm > 0:
+                util.clip_grad_norm(optimizer, max_grad_norm)
+            optimizer.step()
+            scheduler.step(iter_step)
+
+            progress_bar.set_postfix(nll=loss_meter.avg,
+                                     bpd=util.bits_per_dim(x, loss_meter.avg),
+                                     lr=optimizer.param_groups[0]['lr'])
+            progress_bar.update(x.size(0))
+            iter_step += x.size(0)
+            
+@torch.no_grad()
+def test(epoch, net, testloader, device, loss_fn, num_samples):
+    net.eval()
+    loss_meter = util.AverageMeter()
+    with tqdm(total=len(testloader.dataset)) as progress_bar:
+        for x, _ in testloader:
+            x = x.to(device)
+            z, sldj = net(x, reverse=False)
+            loss = loss_fn(z, sldj)
+            loss_meter.update(loss.item(), x.size(0))
+            progress_bar.set_postfix(nll=loss_meter.avg,
+                                     bpd=util.bits_per_dim(x, loss_meter.avg))
+            progress_bar.update(x.size(0))
+
+    # Save checkpoint
+    if loss_meter.avg < net.best_loss:
+        print('Saving...')
+        state = {
+            'net': net.state_dict(),
+            'test_loss': loss_meter.avg,
+            'epoch': epoch,
+        }
+        os.makedirs('ckpts', exist_ok=True)
+        torch.save(state, 'ckpts/best.pth.tar')
+        net.best_loss = loss_meter.avg
+
+    # Save samples and data
+    images = sample(net, num_samples, device)
+    os.makedirs('samples', exist_ok=True)
+    images_concat = torchvision.utils.make_grid(images, nrow=int(num_samples ** 0.5), padding=2, pad_value=255)
+    torchvision.utils.save_image(images_concat, 'samples/epoch_{}.png'.format(epoch))
 
 
 class _Glow(nn.Module):
