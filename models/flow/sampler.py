d1d180997b21a156d5280b962b3d12f914d9d24e
Jun Li <junli19860919@gmail.com>
2021-03-27 15:18:43 -0700

    Create sampler.py

---
+++ models/flow/sampler.py
@@ -0,0 +1,15 @@
+import torch
+
+@torch.no_grad()
+def sample(net, batch_size, device):
+    """Sample from RealNVP model.
+    Args:
+        net (torch.nn.DataParallel): The RealNVP model wrapped in DataParallel.
+        batch_size (int): Number of samples to generate.
+        device (torch.device): Device to use.
+    """
+    z = torch.randn((batch_size, 3, 32, 32), dtype=torch.float32, device=device)
+    x, _ = net(z, reverse=True)
+    x = torch.sigmoid(x)
+
+    return x
