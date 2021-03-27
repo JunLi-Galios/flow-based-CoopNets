06dc93801492b0bb4145a568ad551d9623da580d
Jun Li <junli19860919@gmail.com>
2021-03-27 11:40:35 -0700

    Update ebm.py

--- models/EBM/ebm.py
+++ models/EBM/ebm.py
@@ -1,34 +1,19 @@
 import torch.nn as nn
 
-class Generator(nn.Module):
-    def __init__(self, opt):
-        super(Generator, self).__init__()
-        self.convt1 = nn.ConvTranspose2d(opt.z_size, 512, kernel_size=4, stride=1, padding=0)
-        self.convt2 = nn.ConvTranspose2d(512, 256, kernel_size=5, stride=2, padding=2, output_padding=1)
-        self.convt3 = nn.ConvTranspose2d(256, 128, kernel_size=5, stride=2, padding=2, output_padding=1)
-        self.convt4 = nn.ConvTranspose2d(128, 64, kernel_size=5, stride=2, padding=2, output_padding=1)
-        self.convt5 = nn.ConvTranspose2d(64, 3, kernel_size=5, stride=2, padding=2, output_padding=1)
-        self.bn1 = nn.BatchNorm2d(512)
-        self.bn2 = nn.BatchNorm2d(256)
-        self.bn3 = nn.BatchNorm2d(128)
-        self.bn4 = nn.BatchNorm2d(64)
-        self.leakyrelu = nn.LeakyReLU()
-        self.tanh = nn.Tanh()
-
-    def forward(self, z):
-        self.z = z
-        out = self.convt1(z)
-        out = self.bn1(out)
-        out = self.leakyrelu(out)
-        out = self.convt2(out)
-        out = self.bn2(out)
-        out = self.leakyrelu(out)
-        out = self.convt3(out)
-        out = self.bn3(out)
-        out = self.leakyrelu(out)
-        out = self.convt4(out)
-        out = self.bn4(out)
-        out = self.leakyrelu(out)
-        out = self.convt5(out)
-        out = self.tanh(out)
-        return out
+class F(nn.Module):
+    def __init__(self, n_c=n_ch, n_f = n_f, l= 0.2):
+        super(F, self).__init__()
+        self.f = nn.Sequential(
+            nn.Conv2d(n_c, n_f, 3, 1, 1),
+            nn.LeakyReLU(l),
+            nn.Conv2d(n_f, n_f ∗ 2 , 4, 2, 1),
+            nn.LeakyReLU(l),
+            nn.Conv2d(n_f ∗ 2, n_f ∗ 4, 4, 2, 1),
+            nn.LeakyReLU(l),
+            nn.Conv2d(n_f ∗ 4, n_f ∗ 8, 4, 2, 1),
+            nn.LeakyReLU(l),
+            nn.Conv2d(n_f ∗ 8, 1, 4, 1, 0) 
+        )
+        
+    def forward(self, x):
+        return self.f(x).squeeze()
