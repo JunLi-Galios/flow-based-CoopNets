e2dcc46db70b8503d7fab2493c6834138954a8a6
Jun Li <junli19860919@gmail.com>
2021-03-27 15:29:20 -0700

    Create sampler.py

---
+++ models/EBM/sampler.py
@@ -0,0 +1,10 @@
+import torch
+
+@torch.no_grad()
+def sample(net, m=64, n_ch=3, im_sz=32, im_sz=32, K=100, device):
+    sample_p_0 = lambda: torch.FloatTensor(m, n_ch, im_sz, im_sz).uniform_(-1, 1).to(device)
+    x_k = torch.autograd.Variable(sample_p_0(), requires_grad=True)
+    for k in range(K):
+        net_prime = torch.autograd.grad(net(x_k).sum(), [x_k], retain_graph=True)[0]
+        x_k.data += net_prime + 1e-2 * torch.randn_like(x_k)
+    return x_k.detach()
