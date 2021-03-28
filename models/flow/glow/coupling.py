2f362d1315452075646a06ddea89294e6c437d2e
Jun Li <junli19860919@gmail.com>
2021-03-27 20:28:28 -0700

    Update coupling.py

--- models/flow/glow/coupling.py
+++ models/flow/glow/coupling.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from models.glow.act_norm import ActNorm
+from models.flow.glow.act_norm import ActNorm
 
 
 class Coupling(nn.Module):
