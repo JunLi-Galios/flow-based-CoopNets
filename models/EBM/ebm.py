71cd92addd4955ce11e9cea9597b92a4df41f244
Jun Li <junli19860919@gmail.com>
2021-03-28 22:12:32 -0700

    Update ebm.py

--- models/EBM/ebm.py
+++ models/EBM/ebm.py
@@ -1,19 +1,18 @@
 import torch.nn as nn
 
 class F(nn.Module):
-    def __init__(self, n_c, n_f, l= 0.2):
+    def __init__(self, n_c, n_f, l=0.2):
         super(F, self).__init__()
         self.f = nn.Sequential(
             nn.Conv2d(n_c, n_f, 3, 1, 1),
             nn.LeakyReLU(l),
-            nn.Conv2d(n_f, n_f ∗ 2 , 4, 2, 1),
+            nn.Conv2d(n_f, n_f * 2, 4, 2, 1),
             nn.LeakyReLU(l),
-            nn.Conv2d(n_f ∗ 2, n_f ∗ 4, 4, 2, 1),
+            nn.Conv2d(n_f * 2, n_f * 4, 4, 2, 1),
             nn.LeakyReLU(l),
-            nn.Conv2d(n_f ∗ 4, n_f ∗ 8, 4, 2, 1),
+            nn.Conv2d(n_f * 4, n_f * 8, 4, 2, 1),
             nn.LeakyReLU(l),
-            nn.Conv2d(n_f ∗ 8, 1, 4, 1, 0) 
-        )
-        
+            nn.Conv2d(n_f * 8, 1, 4, 1, 0))
+
     def forward(self, x):
         return self.f(x).squeeze()
